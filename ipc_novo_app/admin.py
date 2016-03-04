a5426ae16e4b51dbaca561c427774d0131a88085
eucandre <eucandre@gmail.com>
2016-03-03 22:51:48 -0300

    mexendo aos poucos

--- ipc_novo_app/admin.py
+++ ipc_novo_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from ipc_novo_app.models import *
 
-# Register your models here.
+admin.site.register(item)
+admin.site.register(subitem)
+admin.site.register(pesos_grupos)
+admin.site.register(subgrupo)
